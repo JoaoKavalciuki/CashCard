2cdd2e2c185c60f1e4e26f910a7ff378796a0562
João <joaokavalciuki@hotmail.com>
2023-12-22 17:09:06 -0300

    Função: CrudRepository criado corretamente, configuração do application.yml feita e tabela e dados para teste adicionados"

--- src/main/java/com/cashcard/cashcard/repositories/CashCardRepository.java
+++ src/main/java/com/cashcard/cashcard/repositories/CashCardRepository.java
@@ -1,6 +1,10 @@
 package com.cashcard.cashcard.repositories;
 
+import com.cashcard.cashcard.CashCard;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CashCardRepository extends CrudRepository {
+@Repository
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
