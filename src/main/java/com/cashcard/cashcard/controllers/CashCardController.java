139f64e4b8efda92486ca4220bed9505a9e004b9
João <joaokavalciuki@hotmail.com>
2023-12-22 17:16:03 -0300

    Função: método GET devidamente criado

---
+++ src/main/java/com/cashcard/cashcard/controllers/CashCardController.java
@@ -0,0 +1,30 @@
+package com.cashcard.cashcard.controllers;
+
+import com.cashcard.cashcard.CashCard;
+import com.cashcard.cashcard.repositories.CashCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+    private final CashCardRepository repository;
+
+    @Autowired
+    private CashCardController(CashCardRepository repository){
+        this.repository = repository;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CashCard> findById(@PathVariable Long id){
+        Optional<CashCard> cashCard = repository.findById(id);
+
+        return cashCard.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
