d22dd4e14855e1e5bf65ea5c89a6e42038ee668d
João <joaokavalciuki@hotmail.com>
2023-12-24 14:24:58 -0300

    Função: método POST nulo adicionado

--- src/main/java/com/cashcard/cashcard/controllers/CashCardController.java
+++ src/main/java/com/cashcard/cashcard/controllers/CashCardController.java
@@ -4,11 +4,7 @@ import com.cashcard.cashcard.CashCard;
 import com.cashcard.cashcard.repositories.CashCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.Optional;
@@ -21,6 +17,11 @@ public class CashCardController {
     @Autowired
     private CashCardController(CashCardRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createCashCard(){
+        return null;
     }
 
     @GetMapping("/{id}")
