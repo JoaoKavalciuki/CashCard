060535c79da83488d6969fcb4eb4604d7c9b7c84
João <joaokavalciuki@hotmail.com>
2023-12-25 17:02:58 -0300

    Função: Método POST de cartões e DTO criada

--- src/main/java/com/cashcard/cashcard/controllers/CashCardController.java
+++ src/main/java/com/cashcard/cashcard/controllers/CashCardController.java
@@ -1,12 +1,15 @@
 package com.cashcard.cashcard.controllers;
 
 import com.cashcard.cashcard.CashCard;
+import com.cashcard.cashcard.dto.CashCardDTO;
 import com.cashcard.cashcard.repositories.CashCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -20,8 +23,13 @@ public class CashCardController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createCashCard(){
-        return null;
+    public ResponseEntity<Void> createCashCard(@RequestBody CashCardDTO newCashCard, UriComponentsBuilder ucb){
+        CashCard savedCashCard = new CashCard(newCashCard);
+        repository.save(savedCashCard);
+
+        URI newCashCardLocation = ucb.path("/cashcards/{id}").buildAndExpand(savedCashCard.getId()).toUri();
+
+        return  ResponseEntity.created(newCashCardLocation).build();
     }
 
     @GetMapping("/{id}")
---
+++ src/main/java/com/cashcard/cashcard/dto/CashCardDTO.java
@@ -0,0 +1,4 @@
+package com.cashcard.cashcard.dto;
+
+public record CashCardDTO(Double amount) {
+}
