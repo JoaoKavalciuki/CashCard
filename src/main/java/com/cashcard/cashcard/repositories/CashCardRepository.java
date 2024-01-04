1a9d7ad897c4ec980b5da88e58ce2226c6bacd06
João <joaokavalciuki@hotmail.com>
2024-01-04 11:40:14 -0300

    Refatoração: CrudRepository mudado para JpaRepository

--- src/main/java/com/cashcard/cashcard/repositories/CashCardRepository.java
+++ src/main/java/com/cashcard/cashcard/repositories/CashCardRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends JpaRepository<CashCard, Long> {
 }
