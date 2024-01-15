235aeb8f585ebcc86233e5eb8cb18fbfe121a409
João <joaokavalciuki@hotmail.com>
2024-01-15 13:50:44 -0300

    Função: Usuário em memória para testes adicionado

--- src/main/java/com/cashcard/cashcard/config/security/SecurityConfig.java
+++ src/main/java/com/cashcard/cashcard/config/security/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,5 +28,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService testsUsers(PasswordEncoder encoder){
+        User.UserBuilder users = User.builder();
+        UserDetails jason = users
+                .username("Jason")
+                .password(encoder.encode("12345"))
+                .roles()
+                .build();
+        return new InMemoryUserDetailsManager(jason);
     }
 }
