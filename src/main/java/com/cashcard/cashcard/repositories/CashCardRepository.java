765ed22070f10dd0c03cf1fc8af457d2b6813765
João <joaokavalciuki@hotmail.com>
2024-01-23 13:37:11 -0300

    Função: métodos para buscar cartões pelos seus donos adicionados

--- src/main/java/com/cashcard/cashcard/repositories/CashCardRepository.java
+++ src/main/java/com/cashcard/cashcard/repositories/CashCardRepository.java
@@ -1,10 +1,16 @@
 package com.cashcard.cashcard.repositories;
 
 import com.cashcard.cashcard.CashCard;
+import com.cashcard.cashcard.dto.ResponseCashCardDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CashCardRepository extends JpaRepository<CashCard, Long> {
+    ResponseCashCardDTO findCashCardByOwner(Long id, String owner);
+
+    Page<ResponseCashCardDTO> findAllByOwner(String owner, PageRequest pageRequest);
 }
